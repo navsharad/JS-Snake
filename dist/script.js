4aba5f8d3a9c7babe7019bd347f56f8da6924378
Nav Sharad <sharadnav@gmail.com>
2021-02-10 14:57:39 -0800

    fixed multiple apple bug

--- dist/script.js
+++ dist/script.js
@@ -3,7 +3,7 @@ const squares = document.querySelectorAll('.grid div');
 const scoreDisplay = document.querySelector('span');
 const startBtn = document.querySelector('.start');
 const endGameModal = document.querySelector('.endgame-modal');
-const endGameScore = document.querySelector('.endgame-modal h4 span');
+const endGameScore = document.querySelector('.endgame-modal h3 span');
 const endGameHighScore = document.querySelector('.endgame-modal h2 span');
 
 const width = 10;
@@ -12,8 +12,7 @@ let appleIndex = 0;
 let currentSnake = [2,1,0];
 let direction = 1;
 let score = 0;
-let speed = 0.9;
-let intervalTime = 0;
+let intervalTime = 200;
 let interval = 0;
 
 
@@ -30,7 +29,6 @@ let interval = 0;
     randomApple();
     direction = 1;
     scoreDisplay.innerText = score;
-    intervalTime = 1000;
     currentSnake = [2,1,0];
     currentIndex = 0;
     currentSnake.forEach(index => squares[index].classList.add('snake'));
@@ -40,30 +38,30 @@ let interval = 0;
 
   // deals with 
   function moveOutcomes() {
-    //deals with snake hitting border and snake hitting self
+    // endgame moves
     if (
-      (currentSnake[0] + width >= (width * width) && direction === width ) || //if snake hits bottom
-      (currentSnake[0] % width === width -1 && direction === 1) || //if snake hits right wall
-      (currentSnake[0] % width === 0 && direction === -1) || //if snake hits left wall
-      (currentSnake[0] - width < 0 && direction === -width) ||  //if snake hits the top
-      squares[currentSnake[0] + direction].classList.contains('snake') //if snake goes into itself
+      (currentSnake[0] + width >= (width * width) && direction === width ) || // snake hits bottom
+      (currentSnake[0] % width === width -1 && direction === 1) || // snake hits right wall
+      (currentSnake[0] % width === 0 && direction === -1) || //snake hits left wall
+      (currentSnake[0] - width < 0 && direction === -width) ||  // snake hits the top
+      squares[currentSnake[0] + direction].classList.contains('snake') // snake crashes into itself
     ) {
       gameGrid.classList.add('hidden');
       endGameModal.classList.remove('hidden');
       endGameScore.textContent = score;
-
       if (localStorage.getItem("highscore") == null || score > localStorage.getItem("highscore")) {
         localStorage.setItem("highscore", score);
       }
       endGameHighScore.textContent = localStorage.getItem("highscore");
-      return clearInterval(interval); //this will clear the interval if any of the above happen
+      return clearInterval(interval);
     }
 
-    const tail = currentSnake.pop(); //removes last ite of the array and shows it
-    squares[tail].classList.remove('snake');  //removes class of snake from the TAIL
-    currentSnake.unshift(currentSnake[0] + direction); //gives direction to the head of the array
+    // changing snake position
+    const tail = currentSnake.pop();
+    squares[tail].classList.remove('snake');
+    currentSnake.unshift(currentSnake[0] + direction);
 
-    //deals with snake getting apple
+    // when snake eats apple
     if(squares[currentSnake[0]].classList.contains('apple')) {
       squares[currentSnake[0]].classList.remove('apple');
       squares[tail].classList.add('snake');
@@ -71,9 +69,6 @@ let interval = 0;
       randomApple();
       score++;
       scoreDisplay.textContent = score;
-      clearInterval(interval);
-      intervalTime = intervalTime * speed;
-      interval = setInterval(moveOutcomes, intervalTime);
     }
     squares[currentSnake[0]].classList.add('snake');
   }
@@ -82,7 +77,10 @@ let interval = 0;
 
   //generate new apple
   function randomApple() {
-    squares[Math.floor(Math.random() * squares.length)].classList.add('apple');
+    do{
+      appleIndex = Math.floor(Math.random() * squares.length);
+    } while(squares[appleIndex].classList.contains('snake'));
+    squares[appleIndex].classList.add('apple')
   }
 
 
@@ -101,5 +99,5 @@ let interval = 0;
     }
   }
 
-  document.addEventListener('keyup', moveSnake);
   startBtn.addEventListener('click', startGame);
+  document.addEventListener('keyup', moveSnake);
